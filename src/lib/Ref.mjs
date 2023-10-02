3e4b57541915f81f8ad7d229ef12247d6c0970f0
Allen Francis <allen@superhire.net>
2023-10-02 20:36:32 +0530

    feat: implement reactive primitive ref

---
+++ src/lib/Ref.mjs
@@ -0,0 +1,56 @@
+/**
+ * Reactive ref primitive implementing observable
+ */
+export class Ref {
+    /**
+     * Instantiate a new ref
+     *
+     * @param {any} value Value of the ref
+     */
+    constructor(value) {
+        this._value = value;
+        this._watchers = new Set();
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(val) {
+        this._value = val;
+
+        this._executeWatchers();
+    }
+
+    _executeWatchers() {
+        for (const watcher of this._watchers) {
+            requestAnimationFrame(() => {
+                watcher(this._value);
+            });
+        }
+    }
+
+    /**
+     * Add a watcher to get triggered when value of the
+     * ref changes
+     *
+     * @param {(value: any) => any} watcher
+     */
+    watch(watcher) {
+        if (typeof watcher !== "function") {
+            throw new InvalidWatcherError();
+        }
+
+        this._watchers.add(watcher);
+
+        return () => {
+            this._watchers.delete(watcher);
+        };
+    }
+}
+
+export class InvalidWatcherError extends Error {
+    constructor(msg) {
+        super(msg);
+    }
+}
